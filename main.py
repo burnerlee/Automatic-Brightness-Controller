6e42f98f1d6b50a6f2aeb48867daedf7cf078ca1
burnerlee <avi.aviral140@gmail.com>
2020-04-05 23:03:46 +0530

    Add GUI interface
    Add GUI with user to configure the
    sensitivity and other settings of the
    program.

--- main.py
+++ main.py
@@ -3,86 +3,214 @@ import os
 import time
 import signal
 import sys
+import threading
+
+try:
+    from PyQt4 import QtGui,QtCore
+except:
+    print "\nInstall PyQt4 using sudo apt install python-qt4\n"
+    exit(0)
+
 try:
     import pyscreenshot as ImageGrab
 except:
     print "\nInstall Pyscreenshot from pip using `pip install pyscreenshot`\n"
     exit(0)
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+# Print instruction messages to guide the user in the beginning
+
 print "Welcome to Automatic Brightness Adjuster for you and your eyes"
 print "Congrats You are on the right path to save your eyes from damage\n"
 print "Usage:"
-print "python main.py [pixel_gap] [time-interval]\n"
-print "pixel-gap(positive integer value)-> spacing between pixels to calculate brightness of screen"
-print "high pixel gap means low accuracy but low computational cost and vice-versa. Default value: 3 pixels\n"
-print "time-interval(positive value > 1)-> time interval between adjusting your screen brightness. Recommended 1.0. Default: 1.0 sec\n"
+print "Pixel-gap-> Spacing between pixels to calculate brightness of screen"
+print "High pixel gap means low accuracy but low computational cost and vice-versa.\n"
+print "Time-interval-> Time interval between adjusting your screen brightness.\n"
+print "Sensitivity-> Set sensitivity of change in pixels for which brightness changes"
+print "Low sensitivity would change brightness on larger change in screen colour and vice versa \n"
+
 arguments=sys.argv
-try:
-    if len(arguments)==2:
-        if int(arguments[1])>=1:
-            increment=int(arguments[1])
-        else:
-            print "Please enter a valid value for pixel-gap. Currently set to default"
-            increment=3
-        time_interval=1.0
-    elif len(arguments)>2:
-        if arguments[2]>1:
-            time_interval=float(arguments[2])
-        else:
-            time_interval=1.0
-            print "Please enter a valid value for time-interval . Currently set to default"
-        if int(arguments[1])>1:
-            increment=int(arguments[1])
-        else:
-            print "Please enter a valid value for pixel-gap. Currently set to default"
-            increment=3
-except:
-    print "Please enter valid arguments"
-    print "Exiting..."
-    exit(0)
-else:
-    increment=3
-    time_interval=1.0
+stopped=False
+increment=3
+time_interval=1.0
+sensitivity=1
+
 print "Enjoy! Just leave it on us to save you\n"
 print "Starting ..."
-time.sleep(3)
+
+# Defining the GUI part of the Application
+class Window(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(Window,self).__init__()
+
+        self.setGeometry(50,50,600,300)
+        self.setWindowTitle("Automatic Brightness Controller")
+        self.setWindowIcon(QIcon("logo.jpeg"))
+
+        # Configure the Apply Changes button
+        btn=QtGui.QPushButton("Apply Changes",self)
+        btn.move(220,240)
+        btn.resize(160,30)
+        btn.clicked.connect(self.apply)  
+
+        # Defining the window labels
+        l1=QtGui.QLabel(self)
+        l1.setText("You can Change the Settings and click on Apply Changes")
+        l1.setGeometry(100,10,400,50)
+        l1.setAlignment(Qt.AlignCenter)
+
+        l2=QtGui.QLabel(self)
+        l2.setText("Pixel Gap:")
+        l2.setGeometry(20,75,150,30)
+        l2.setAlignment(Qt.AlignLeft)
+
+        l3=QtGui.QLabel(self)
+        l3.setText("Time Interval:")
+        l3.setGeometry(20,120,150,30)
+        l3.setAlignment(Qt.AlignLeft)
+
+        l4=QtGui.QLabel(self)
+        l4.setText("Change Sensitivity:")
+        l4.setGeometry(20,165,150,30)
+        l4.setAlignment(Qt.AlignLeft)
+
+        l5=QtGui.QLabel(self)
+        l5.setText("Note: Closing this will close the application, Rather minimize to System Tray")
+        l5.setGeometry(10,210,580,20)
+        l5.setAlignment(Qt.AlignCenter)
+
+        global s1,s2,s3                         
+
+        # Defining the three sliders which take input from the user
+        s1=QSlider(Qt.Horizontal,self)
+        s1.setMinimum(3)
+        s1.setMaximum(20)
+        s1.setValue(3)
+        s1.setSingleStep(1)
+        s1.setTickPosition(QSlider.TicksBelow)
+        s1.setTickInterval(1)
+        s1.move(200,80)
+        s1.resize(300,30)
+
+        s2=QSlider(Qt.Horizontal,self)
+        s2.setMinimum(1)
+        s2.setMaximum(20)
+        s2.setValue(1)
+        s2.move(200,125)
+        s2.resize(300,30)
+        s2.setSingleStep(1)
+        s2.setTickPosition(QSlider.TicksBelow)
+        s2.setTickInterval(1)
+
+        s3=QSlider(Qt.Horizontal,self)
+        s3.setMinimum(1)
+        s3.setMaximum(100)
+        s3.setValue(1)
+        s3.move(200,170)
+        s3.resize(300,30)
+        s3.setSingleStep(1)
+        s3.setTickPosition(QSlider.TicksBelow)
+        s3.setTickInterval(5)    
+
+        self.show()
+
+    # apply is execute when user clicks the Apply Changes button
+    def apply(self):
+        print "Changes Applied"
+        
+        # Change the values for adjusting brightness as entered by the user 
+        global increment,time_interval,sensitivity
+        increment= s1.value()
+        time_interval = s2.value()
+        sensitivity = s3.value()
+
+        # Add the alert to confirm that changes are made
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("Changes Applied")
+        msg.setWindowTitle("Alert")
+        msg.setStandardButtons(QMessageBox.Ok)
+	    
+        retval = msg.exec_()
+    
+    # closeEvent is defined to stop the application when window is closed
+    def closeEvent(self, *args, **kwargs):
+        super(QtGui.QMainWindow, self).closeEvent(*args, **kwargs)
+        global stopped
+        stopped=True
+
+# Define the GUI 
+app=QtGui.QApplication(sys.argv)
+GUI=Window()
+
+# Define initial screen variables and other constants 
 SCREEN = ImageGrab.grab()
 SCREEN_SIZE=SCREEN.size
 MAX_X = SCREEN_SIZE[0]
 MAX_Y = SCREEN_SIZE[1]
 BRIGHTNESS_DEFAULT=1.0
-brightness_set=BRIGHTNESS_DEFAULT
 stream = os.popen("xrandr -q | grep ' connected' | head -n 1 | cut -d ' ' -f1")
 DESKTOP_NAME = stream.readline()
 DESKTOP_NAME = DESKTOP_NAME.rstrip() 
+
+# exit_function is called when program exits
 def exit_function():
     command = "xrandr --output "+ DESKTOP_NAME +" --brightness 1"
     os.system(command)
     print "\n\nBYE BYE!"
-    print "Brightness set to normal!"   
+    print "Brightness set to normal!" 
+
+# define a function if program is interrupted  
 def keyboardInterruptHandler(signal, frame):
-    exit_function()
-    exit(0)
+    exit_function() 
+
 signal.signal(signal.SIGINT, keyboardInterruptHandler)
-while 1:
-    sum_brightness=0
-    current_screen = ImageGrab.grab()
-    for x in range (0,MAX_X,increment):
-        for y in range (0,MAX_Y,increment):
-            cordinate=x,y
-            rgb = (current_screen.getpixel(cordinate))
-            R=rgb[0]
-            G=rgb[1]
-            B=rgb[2]
-            brightness=math.sqrt(0.299*R*R + 0.587*G*G + 0.114*B*B )
-            sum_brightness+=brightness
-    average_brightness=sum_brightness/MAX_X/MAX_Y*increment*increment
-    difference=255-average_brightness
-    mean=difference/255
-    if((mean+0.3)>(brightness_set+0.1) or (mean+0.3)<(brightness_set-0.1)):
-        brightness_set=0.3+mean
-        print "Brightness set to " + str(brightness_set)
-        command = "xrandr --output "+ DESKTOP_NAME +" --brightness "+str(brightness_set)
-        os.system(command)
-    else:
-        print "Brightness same"
-    time.sleep(float(time_interval))
+
+# adjust_brightness calculate the current state of the screen and adjusts brightness accordingly
+def adjust_brightness(brightness_set):
+    time.sleep(5)
+    while 1:
+        global stopped,increment,sensitivity,time_interval
+
+        if stopped:
+            break
+
+        sum_brightness=0
+        current_screen = ImageGrab.grab()
+
+        for x in range (0,MAX_X,increment):
+            for y in range (0,MAX_Y,increment):
+                
+                cordinate=x,y
+                rgb = (current_screen.getpixel(cordinate))
+                R=rgb[0]
+                G=rgb[1]
+                B=rgb[2]
+                brightness=math.sqrt(0.299*R*R + 0.587*G*G + 0.114*B*B )            #formula to derive brightness of a pixel from RGB
+                sum_brightness+=brightness
+
+        average_brightness=sum_brightness/MAX_X/MAX_Y*increment*increment           #average value of brightness
+        difference=255-average_brightness
+        mean=difference/255
+        sensitivity_diff = 100 - sensitivity
+        sensitivity_mean =float(sensitivity_diff)/1000
+
+        if(float(mean+0.3)>float(brightness_set + sensitivity_mean) or float(mean+0.3)<float(brightness_set-sensitivity_mean)):
+            brightness_set=0.3+mean
+            print "Brightness set to " + str(brightness_set)
+            command = "xrandr --output "+ DESKTOP_NAME +" --brightness "+str(brightness_set)
+            os.system(command)
+        else:
+            print "Brightness is same"
+        time.sleep(float(time_interval))
+
+    exit_function()
+# The adjust_brightness function is called on the thread t1
+t1=threading.Thread(target=adjust_brightness,args=(BRIGHTNESS_DEFAULT,))
+t1.start()
+
+#GUI is executed and the adjust_brightness function runs on the thread t1
+app.exec_()
